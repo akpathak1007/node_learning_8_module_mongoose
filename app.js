13df2de67ab5f7acee23c031702443eb1957d798
akpathak1007 <anujkumarpathak1007@gmail.com>
2022-02-17 10:15:26 +0530

    refactored routers

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const { get } = require('express/lib/response');
 const fs = require('fs');
 
 const app = express();
+// ? MIDDLEWARES
 app.use(express.json());
 /* Reading tours data from a json file */
 const jsonFile = `${__dirname}/dev-data/data/tours-simple.json`;
 const tours = JSON.parse(fs.readFileSync(jsonFile, 'utf-8'));
 
+// ? ROUTE HANDLERS
 // Todo: Delete tour
 const delete_tour = (req, res) => {
   return res.status(204).json({
@@ -15,7 +17,7 @@ const delete_tour = (req, res) => {
     data: null,
   });
 };
-
+// Todo: Create a new Toure
 const new_tour = (req, res) => {
   const body = req.body;
   const id = tours[tours.length - 1].id + 1;
@@ -32,7 +34,6 @@ const new_tour = (req, res) => {
     });
   });
 };
-
 // Todo: Get a single tours
 /* To define default params using ? . example /api/v1/tours/:id/userId?  */
 const get_single_tour = (req, res) => {
@@ -52,7 +53,6 @@ const get_single_tour = (req, res) => {
     },
   });
 };
-
 // Todo: Get all tours
 const get_all_tours = (req, res) => {
   res.status(200).json({
@@ -63,7 +63,6 @@ const get_all_tours = (req, res) => {
     },
   });
 };
-
 // Todo: Update a tour
 const update_tour = (req, res) => {
   try {
@@ -83,20 +82,66 @@ const update_tour = (req, res) => {
     });
   }
 };
-// Todo: Routes
-/* app.post('/api/v1/tour', new_tour);
-app.get('/api/v1/tours', get_all_tours);
-app.get('/api/v1/tours/:id', get_single_tour);
-app.patch('/api/v1/tours/:id', update_tour);
-app.delete('/api/v1/tours/:id', delete_tour);
- */
-app.route('/api/v1/tour').get(get_all_tours).post(new_tour);
+// Todo: Create New user
+const new_user = (req, res) => {
+  return res.status(201).json({
+    status: 'SUCCESS',
+    message: 'this route is not implemeted yet.',
+    data: null,
+  });
+};
+// Todo: Get all users
+const get_all_users = (req, res) => {
+  return res.status(200).json({
+    status: 'SUCCESS',
+    message: 'this route is not implemeted yet.',
+    data: null,
+  });
+};
+// Todo: Get a single user
+const get_single_user = (req, res) => {
+  return res.status(200).json({
+    status: 'SUCCESS',
+    message: 'this route is not implemeted yet.',
+    data: null,
+  });
+};
+// Todo: Update user
+const update_user = (req, res) => {
+  return res.status(201).json({
+    status: 'SUCCESS',
+    message: 'this route is not implemeted yet.',
+    data: null,
+  });
+};
+// Todo: Delete user
+const delete_user = (req, res) => {
+  return res.status(201).json({
+    status: 'SUCCESS',
+    message: 'this route is not implemeted yet.',
+    data: null,
+  });
+};
+
+// ? SEPARATE ROUTERS
+const userRouter = express.Router();
+const tourRouter = express.Router();
+
+
+//  ? ROUTES
+app.route('/api/v1/tours').get(get_all_tours).post(new_tour);
 app
   .route('/api/v1/tours/:id')
   .get(get_single_tour)
   .patch(update_tour)
   .delete(delete_tour);
 
+app.route('/api/v1/users').get(get_all_users).post(new_user);
+app
+  .route('/api/v1/users/:id')
+  .get(get_single_user)
+  .patch(update_user)
+  .delete(delete_user);
 
 const port = 3000;
 app.listen(port, () => {
